07620fe3279e56d7561a2da4cfe18b1ae35d2f70
Yunno42 <tk9651756@gmail.com>
2021-04-23 20:50:31 -0300

    First commit

---
+++ Yunno/src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comando Da 🥀Yunno🥀*    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.toinmenu = yunnomenu
+   
